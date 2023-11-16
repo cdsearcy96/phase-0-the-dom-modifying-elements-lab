07446ab00e4942731099442d0cf8a8405585ae98
+ <+>
2023-11-15 21:32:53 -0800

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set the id of the <h1> element to "victory"
+newHeader.id = 'victory';
+
+// Set the inner text or HTML content of the <h1> element
+newHeader.innerHTML = 'Cierra is the champion';
